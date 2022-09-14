671a6f9ba0cc8173338b1f6cc3c30552618c61a3
LilNiclas <nicl2995@stud.kea.dk>
2022-09-14 12:13:42 +0200

    9. Pæn sourcecode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,80 +3,92 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    Database database = new Database();
+    Scanner scan = new Scanner(System.in);
+    int count = 1;
+    int startside;
 
-    public void Startside(){
+    public void userStart() {
+        do {
+            System.out.println("Velkommen til MySuperheroList");
+            System.out.println("1. Opret Superhelte");
+            System.out.println("3. Superhelte register");
+            System.out.println("5. Søg helte");
+            System.out.println("9. Luk programmet");
+            System.out.println("-----------------------------");
+            startside = scan.nextInt();
+            scan.nextLine();
 
+            if (startside == 1) {
+                opretHelt();
+            } else if (startside == 3) {
+                superhelteRegister();
+            } else if (startside == 5) {
+                søgHelte();
+            } else if (startside == 9) {
+                System.out.println("Lukker program");
+                System.exit(0);
+            }
+        } while (startside != 9);
+    }
+
+    public void opretHelt() {
+        if (startside == 1) {
+            System.out.println("Opret en superhelt (" + count + ")");
+            count++;
+
+            System.out.print("Indtast superhelt navn: ");
+            String navn = scan.nextLine();
+
+            System.out.print("Indtast superkraft: ");
+            String superkraft = scan.nextLine();
+
+            System.out.print("Er helten et menneske (j eller n: )");
+            boolean menneske = true;
+            char humanSvar = scan.nextLine().charAt(0);
+            if (humanSvar == 'n') {
+                menneske = false;
+            } else if (humanSvar == 'j') {
+                menneske = true;
+            } else {
+                System.out.println("ugyldigt input");
+            }
+
+            System.out.print("Heltens introduktionsår: ");
+            int introår = scan.nextInt();
+            System.out.print("Heltens styrkepoint (et normalt menneske er 1.0): ");
+            double styrkepoint = scan.nextDouble();
+            System.out.println(" ");
+
+            database.createSuperhero(navn, superkraft, menneske, introår, styrkepoint);
+        }
+
+
+    }
+
+    public void superhelteRegister() {
+        if (startside == 3) {
+            for (Superhero helt : database.getHelteDatabase()) {
+
+                System.out.println("Superhelte navn: " + helt.getNavn());
+                System.out.println("Superkraft: " + helt.getSuperkraft());
+                System.out.println("Er helt menneske?" + helt.isMenneske());
+                System.out.println("Introduktionsår: " + helt.getIntroår());
+                System.out.println("Styrkepoint: " + helt.getStyrkepoint());
+            }
+        }
+    }
+
+    public void søgHelte() {
+        if (startside == 5) {
+            String searchTerm = scan.nextLine();
+            Superhero superhero = database.searchFor(searchTerm);
+            System.out.println(superhero);
+        }
     }
 
     public static void main(String[] arg) {
-        Database database = new Database();
-        int count = 1;
-        int startside;
-        System.out.println("Velkommen til MySuperheroList");
-        Scanner scan = new Scanner(System.in);
-
-        do {
-            System.out.println("1. Opret Superhelte");
-            System.out.println("3. Superhelte register");
-            System.out.println("9. Luk programmet");
-            startside = scan.nextInt();
-            scan.nextLine();
-            System.out.println("-----------------------------");
-
-
-            if (startside == 1) {
-                System.out.println("Opret en superhelt (" + count + ")");
-                count++;
-
-                System.out.print("Indtast superhelt navn: ");
-                String navn = scan.nextLine();
-
-                System.out.print("Indtast superkraft: ");
-                String superkraft = scan.nextLine();
-
-                System.out.print("Er helten et menneske (j eller n: )");
-                boolean menneske = true;
-                char humanSvar = scan.nextLine().charAt(0);
-                if (humanSvar == 'n') {
-                    menneske = false;
-                } else if (humanSvar == 'j') {
-                    menneske = true;
-                } else {
-                    System.out.println("ugyldigt input");
-                }
-
-                System.out.print("Heltens introduktionsår: ");
-                int introår = scan.nextInt();
-                System.out.print("Heltens styrkepoint (et normalt menneske er 1.0): ");
-                double styrkepoint = scan.nextDouble();
-                System.out.println(" ");
-
-                database.createSuperhero(navn, superkraft, menneske, introår, styrkepoint);
-            }
-
-            if (startside == 3) {
-                for (Superhero helt : database.getHelteDatabase()) {
-
-                    System.out.println("Superhelte navn: " + helt.getNavn());
-                    System.out.println("Superkraft: " + helt.getSuperkraft());
-                    System.out.println("Er helt menneske?" + helt.isMenneske());
-                    System.out.println("Introduktionsår: " + helt.getIntroår());
-                    System.out.println("Styrkepoint: " + helt.getStyrkepoint());
-                }
-            }
-
-            if (startside == 5) {
-                String searchTerm = scan.nextLine();
-                Superhero superhero = database.searchFor(searchTerm);
-                System.out.println(superhero);
-            }
-
-            if (startside == 9) {
-                System.out.println("Lukker programmet.. .");
-            }
-
-        } while (startside != 9);
+        Main program = new Main();
+        program.userStart();
     }
-}
-
-
+    }
